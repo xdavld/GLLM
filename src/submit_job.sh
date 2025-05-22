40961236a52b0a7df0dfdec00566cb81c27c14ec
Niwando <nicowagner02@gmail.com>
2025-05-22 13:14:07 +0200

    general

--- src/submit_job.sh
+++ src/submit_job.sh
@@ -4,13 +4,13 @@ CPUS=1                            # Number of CPUs (per task)
 GPUS=0                            # Number of GPUs
 MEM=4G                            # Memory per node
 TIME="02:00:00"                   # Time limit (hh:mm:ss) / maximum time to run is 72 hours
-QUEUES=CPU                        # Queues to use (e.g., cpu, gpu_h100)
-PYTHON_SCRIPT="test.py"           # Python file to execute (must be in current dir or use full path)
+PARTITION=cpu                    # Partition to use (e.g., cpu, gpu_h100)
+PYTHON_SCRIPT="src/test.py"           # Python file to execute (must be in current dir or use full path)
 ARGS=""
 
-WORKDIR=$(pwd)                    # Current working directory
-VENV_PATH="$WORKDIR/venvs/llm-gan"   # Path to your virtualenv
-LOGDIR="$WORKDIR/logs"
+DIR="/home/ds/ds_ds/ds_wi22230/LLM_GAN_MarketResearch/LLM_GAN_MarketResearch"
+VENV_PATH="$DIR/llm-gan"          # Path to your virtualenv
+LOGDIR="$DIR/logs"
 mkdir -p "$LOGDIR"
 
 JOBNAME="synthetic_data_job_$(date +%Y%m%d%H%M%S)"
@@ -22,11 +22,14 @@ sbatch <<EOF
 #SBATCH --job-name=$JOBNAME
 #SBATCH --output=$LOGFILE
 #SBATCH --error=$LOGFILE
-#SBATCH --queues=$QUEUES
+#SBATCH --partition=$PARTITION
 #SBATCH --gres=gpu:$GPUS
 #SBATCH --cpus-per-task=$CPUS
 #SBATCH --mem=$MEM
 #SBATCH --time=$TIME
+
+# Go to script directory (optional, adjust if needed)
+cd $DIR
 
 # Activate virtual environment
 source $VENV_PATH/bin/activate
@@ -37,4 +40,4 @@ python $PYTHON_SCRIPT $ARGS
 echo "Finished at \$(date)"
 EOF
 
-echo -e "\nJob '$JOBNAME' submitted to queue '$QUEUES'. Logs: $LOGFILE"
+echo -e "\nJob '$JOBNAME' submitted to partition '$PARTITION'. Logs: $LOGFILE"
